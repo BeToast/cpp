41baba697a82879e55ba080bfc73ecf0429f4247
BeToast <note2bp@protonmail.com>
2023-11-20 19:31:39 +0000

    moving to laptop

--- new.cpp
+++ new.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
-using namespace std;
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+  char value { 'c' };
 
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+  std::cout << value;
+
+
+  std::cout << "\n\n\n\n";
+  return 0;
 }
